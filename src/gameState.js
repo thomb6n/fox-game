4ea4567c616a403600f472618d04a3fef884f4d9
Tom de Visser <tom.d6r@outlook.com>
2024-01-19 23:48:00 +0100

    Added rain, hatching and set up more actions on game state

--- src/gameState.js
+++ src/gameState.js
@@ -1,25 +1,67 @@
-// const states = [
-//   "INIT",
-//   "HATCHING",
-//   "IDLING",
-//   "SLEEPING",
-//   "EATING",
-//   "POOPING",
-//   "HUNGRY",
-//   "CELEBRATING",
-//   "DEAD",
-// ];
+import { RAIN_CHANCE, SCENES } from "./constants";
+import { modFox, modScene } from "./ui";
 
 const gameState = {
   current: "INIT",
   clock: 1,
+  wakeTime: -1,
   tick() {
     this.clock++;
     console.log("clock", this.clock);
+
+    if (this.clock === this.wakeTime) {
+      this.wake();
+    }
+  },
+  startGame() {
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+    modFox("egg");
+    modScene("day");
+  },
+  wake() {
+    this.current = "IDLING";
+    this.wakeTime = -1;
+    modFox("idling");
+    this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
+    modScene(SCENES[this.scene]);
+  },
+  changeWeather() {
+    console.log("change weather");
+  },
+  cleanUpPoop() {
+    console.log("clean poop");
+  },
+  feed() {
+    console.log("feed");
   },
   handleUserAction(icon) {
-    console.log(icon);
+    if (
+      ["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)
+    ) {
+      return;
+    }
+
+    if (["DEAD", "INIT"].includes(this.current)) {
+      this.startGame();
+      return;
+    }
+
+    switch (icon) {
+      case "weather":
+        this.changeWeather();
+        break;
+      case "poop":
+        this.cleanUpPoop();
+        break;
+      case "fish":
+        this.feed();
+        break;
+      default:
+        break;
+    }
   },
 };
 
+export const handleUserAction = gameState.handleUserAction.bind(gameState);
 export default gameState;
